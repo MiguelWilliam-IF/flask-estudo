cd11f50d4d07686d519dda4e4208565bf166925b
MiguelWilliam-IF <162115457+MiguelWilliam-IF@users.noreply.github.com>
2024-10-21 10:13:09 -0300

    Aula 21/10

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask # Biblioteca
+from flask import render_template
+from flask import request
 
 app = Flask(__name__) # Carregamento
 
@@ -6,6 +8,16 @@ app = Flask(__name__) # Carregamento
 def index(): # Função
     return '<h1>Olá<br>mundo!</h1>'
 
-@app.route("/aluno/<nome>") # Rota
-def aluno(nome): # Função
-    return f'<h1>meu nome é {nome}</h1>'
+@app.route("/aluno") # Rota
+def aluno(): # Função
+    return render_template('formulario.html')
+
+@app.route("/envio", methods=['POST']) # Rota
+def envioForms(): # Função
+    nombre = request.form['nome']
+    senha = request.form['senha']
+
+    if senha == '123':
+        return render_template('aluno.html', n = nombre)
+    else:
+        return 'NAO PASSARÁ!!!!'
